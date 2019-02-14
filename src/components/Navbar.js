0b09445b04e853c1fd9d66d11e02724f4df10013
Huiling Wu <huwu@ucdavis.edu>
2019-02-14 15:58:09 -0800

    trying to deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends React.Component {
             <nav className="navbar navbar-default fixed-top navbar-light bg-light">
                 <div className="container-fluid">
                     <span className="navbar-brand mb-0 h1">React Clicky Game</span>
-                    <span className="comment">{this.state.comment}</span>
+                    <span className="comment h6">{this.state.comment}</span>
                     <span className="score">score: {this.state.score}</span>
                     <span className="topScore">Top Score: {this.state.topScore}</span>
                 </div>
