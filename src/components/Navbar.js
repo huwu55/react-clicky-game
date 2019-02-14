d151ed25e5d5b501911372c46e9f1d189143d4a1
Huiling Wu <huwu@ucdavis.edu>
2019-02-14 15:10:15 -0800

    added fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,11 +13,13 @@ class Navbar extends React.Component {
 
     render(){
         return (
-            <nav className="navbar navbar-light bg-light">
-                <span className="navbar-brand mb-0 h1">React Clicky Game</span>
-                <span className="comment">{this.state.comment}</span>
-                <span className="score">score: {this.state.score}</span>
-                <span className="topScore">Top Score: {this.state.topScore}</span>
+            <nav className="navbar navbar-default fixed-top navbar-light bg-light">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">React Clicky Game</span>
+                    <span className="comment">{this.state.comment}</span>
+                    <span className="score">score: {this.state.score}</span>
+                    <span className="topScore">Top Score: {this.state.topScore}</span>
+                </div>
             </nav>
         );
     }
