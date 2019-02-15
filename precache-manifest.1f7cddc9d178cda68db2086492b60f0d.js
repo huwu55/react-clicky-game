d37483c7f240b118e3c137162eeaf2ae279cb2e9
Huiling Wu <huwu@ucdavis.edu>
2019-02-14 16:34:28 -0800

    Updates

---
+++ precache-manifest.1f7cddc9d178cda68db2086492b60f0d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "83790587ec6371ad99419a7d0cac86ed",
+    "url": "/react-clicky-game/static/media/reborn-p6.83790587.jpg"
+  },
+  {
+    "revision": "3096e7f87d7299e6d35c",
+    "url": "/react-clicky-game/static/css/main.75c10716.chunk.css"
+  },
+  {
+    "revision": "07c95f7184c1a76ef7a1",
+    "url": "/react-clicky-game/static/js/runtime~main.07c95f71.js"
+  },
+  {
+    "revision": "5c0423f9bdef19d94ebf",
+    "url": "/react-clicky-game/static/js/2.5c0423f9.chunk.js"
+  },
+  {
+    "revision": "a371fb4fc1d17c99f4bac869c79c87dd",
+    "url": "/react-clicky-game/static/media/reborn-p0.a371fb4f.jpg"
+  },
+  {
+    "revision": "ec2a8934e4aeaf3585a5c6a2a3cc031e",
+    "url": "/react-clicky-game/static/media/reborn-p1.ec2a8934.jpg"
+  },
+  {
+    "revision": "13b4e24a08f87775afd242f55cadfe88",
+    "url": "/react-clicky-game/static/media/reborn-p2.13b4e24a.jpg"
+  },
+  {
+    "revision": "1487b7a10e762aea62b1c9f329334e56",
+    "url": "/react-clicky-game/static/media/reborn-p3.1487b7a1.jpg"
+  },
+  {
+    "revision": "67c0371c8ca681286c5a7b01fdf5d9a6",
+    "url": "/react-clicky-game/static/media/reborn-p4.67c0371c.jpg"
+  },
+  {
+    "revision": "5382f582675e659d9c188517d51e5516",
+    "url": "/react-clicky-game/static/media/reborn-p5.5382f582.jpg"
+  },
+  {
+    "revision": "3096e7f87d7299e6d35c",
+    "url": "/react-clicky-game/static/js/main.3096e7f8.chunk.js"
+  },
+  {
+    "revision": "6001493511933ad1ba45ba42bb3c6d10",
+    "url": "/react-clicky-game/static/media/reborn-p7.60014935.jpg"
+  },
+  {
+    "revision": "4c3f4e65da0e645aec1673de6ede3deb",
+    "url": "/react-clicky-game/static/media/reborn-p8.4c3f4e65.jpg"
+  },
+  {
+    "revision": "688ecfba901bf63540b533e7e11a600e",
+    "url": "/react-clicky-game/static/media/reborn-p9.688ecfba.jpg"
+  },
+  {
+    "revision": "8c0bff9efe352e369336843537dfbcba",
+    "url": "/react-clicky-game/static/media/reborn-p10.8c0bff9e.jpg"
+  },
+  {
+    "revision": "07376d39cdd12985968d821c1d14c0e7",
+    "url": "/react-clicky-game/static/media/reborn-p11.07376d39.jpg"
+  },
+  {
+    "revision": "42ca8948c7ee3cd9eb21f3d81e06bec8",
+    "url": "/react-clicky-game/static/media/reborn-p12.42ca8948.jpg"
+  },
+  {
+    "revision": "df074313f3a987dad9a11a5509ced5b4",
+    "url": "/react-clicky-game/static/media/reborn-p13.df074313.jpg"
+  },
+  {
+    "revision": "48ac6712d6310fd06cee8658b928ea4b",
+    "url": "/react-clicky-game/static/media/reborn-p14.48ac6712.jpg"
+  },
+  {
+    "revision": "ea67fef8dcfc713621f1701752f521d4",
+    "url": "/react-clicky-game/static/media/reborn-p15.ea67fef8.jpg"
+  },
+  {
+    "revision": "6b68bb253090336ae4801100eb446d55",
+    "url": "/react-clicky-game/index.html"
+  }
+];
